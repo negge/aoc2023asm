6157bfcbee8680bdf2c615e7ce97ad1d7175172a
Nathan E. Egge <negge@dgql.org>
2023-12-15 09:07:33 -0500

    DOS: d1/p1.com 60 bytes

---
+++ dos/d1/p1.asm
@@ -0,0 +1,44 @@
+; nasm p1.asm -o p1.com
+; dosbox -c "mount D ." -c "D:" -c "dir p1.com" -c "P1.COM < DATA\INPUT.TXT"
+  cpu 8086
+  org 0x100
+start:
+readline:
+  xor cx, cx
+readchar:
+  mov ah, 1
+  int 0x21
+  test al, al
+  jz print
+  sub al, '0'
+  cmp al, 9
+  ja .no
+  test ch, ch
+  jnz .first
+  mov ch, al
+.first:
+  mov cl, al
+.no:
+  cmp al, 10 - '0'
+  jnz readchar
+  xchg ax, cx
+  aad
+  add bx, ax
+  jmp readline
+print:
+  xchg bx, ax
+  mov bx, 10
+.div:
+  xor dx, dx
+  div bx
+  push dx
+  inc cx
+  test ax, ax
+  jnz .div
+  mov ah, 2
+.pop:
+  pop dx
+  add dl, '0'
+  int 0x21
+  loop .pop
+  ret
